ebc1a582f12462a472fdb04389fa5611d44069c2
goldenjeffempire <jeffemuodafe124@gmail.com>
2023-09-14 10:26:38 -0700

    Print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,9 +7,26 @@
   */
 void print_triangle(int size)
 {
-	print_triangle(2);
-	print_triangle(10);
-	print_triangle(1);
-	print_triangle(0);
-	return (0);
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	int num_chars = 1;
+
+	for (int row = 1; row <= size; row++)
+	{
+		for (int space = 1; space <= size - row; space++)
+		{
+			_putchar(' ');
+		}
+
+		for (int column = 1; column <= num_chars; column++)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+		num_chars++;
+	}
 }
