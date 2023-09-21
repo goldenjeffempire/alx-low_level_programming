139aa47d829e05238c48b2c9187f6b4fe916b258
goldenjeffempire <jeffemuodafe124@gmail.com>
2023-09-20 17:40:49 -0700

    Strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,19 +7,19 @@
  * @n : int params
  * Return: *dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *result = dest;
+	int i;
 
-	while (n > 0 && *src != '\0')
+	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
-		*dest = *src;
-		dest++;
-		src++;
-		n--;
+		dest[i] = src[i];
 	}
 
-	*dest = '\0';
-
-	return (result);
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
 }
