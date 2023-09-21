81e13a54d4920183a36ca3aedb095bff5a17ce4f
goldenjeffempire <jeffemuodafe124@gmail.com>
2023-09-21 15:54:19 -0700

    Magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,7 +2,7 @@
 
 /**
   * main - Entry point
-  * Return: Always (0) Success
+  * Return: Always 0
   */
 int main(void)
 {
@@ -12,8 +12,7 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-
-	/**
+	/*
 	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
