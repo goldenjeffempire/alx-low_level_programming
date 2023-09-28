90cc0797eaa2d484eaef73b5ce9c6cf58fd3865b
goldenjeffempire <jeffemuodafe124@gmail.com>
2023-09-28 11:32:21 -0700

    Sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,46 +1,30 @@
 #include "main.h"
 
 /**
-  * _sqrt_recursion - Returns the natural square root of a number
-  * @n: The number to calculate the square root of
-  *
-  * Return: The natural square root of the number or -1 if it does not have one
-  */
-int _sqrt_recursion(int n)
+ * sqrt_check - checks for the square root of c
+ * @g:guess at sqrt
+ * @c: number to find sqrt of
+ *
+ * Return: -1 or sqrt of c
+ */
+int sqrt_check(int g, int c)
 {
-	if (n < 0)
+	if (g * g == c)
+		return (g);
+	if (g * g > c)
 		return (-1);
-
-	if (n == 0 || n == 1)
-		return (n);
-
-	return (sqrt_helper(n, 1, n));
+	return (sqrt_check(g + 1, c));
 }
 
 /**
-  * sqrt_helper - Helper function to calculate the square root recursively
-  * @n: The helper to calculate the square root of
-  * @start: The starting point for the search
-  * @end: The ending point for the search
-  *
-  * Return: The natural square root of the number or -1 if it does not have one
-  */
-int sqrt_helper(int n, int start, int end)
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
+ *
+ * Return: natural square root or -1
+ */
+int _sqrt_recursion(int n)
 {
-	int mid;
-
-	if (start <= end)
-	{
-		mid = (start + end) / 2;
-
-		if (mid * mid == n)
-			return (mid);
-
-		if (mid * mid > n)
-			return (sqrt_helper(n, start, mid - 1));
-
-		return (sqrt_helper(n, mid + 1, end));
-	}
-
-	return (end);
+	if (n == 0)
+		return (0);
+	return (sqrt_check(1, n));
 }
