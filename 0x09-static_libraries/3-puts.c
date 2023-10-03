f1ac1108ba2cf7663740686df2dd3b85af127a3e
goldenjeffempire <jeffemuodafe124@gmail.com>
2023-10-02 18:26:19 -0700

    Libmya

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,11 +1,15 @@
 #include "main.h"
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
-int main(void)
+  * _puts - Function to print a string
+  * @str: To print to the standard output
+  */
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	if (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
 }
