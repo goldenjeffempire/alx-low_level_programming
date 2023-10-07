5ad6682c64920c46ed53370ba10e1ce911833678
goldenjeffempire <jeffemuodafe124@gmail.com>
2023-10-07 09:22:19 -0700

    Realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,29 +12,27 @@
   */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
-
-	if (new_size == 0 && ptr != NULL)
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	if (new_size == old_size)
-		return (ptr);
-
-	new_ptr = malloc(new_size);
+	void *new_ptr = malloc(new_size);
 
 	if (new_ptr == NULL)
+	{
 		return (NULL);
+	}
 
 	if (ptr != NULL)
 	{
-		unsigned int copy_size = old_size;
+		size_t copy_size = old_size;
 
 		if (new_size < old_size)
-			copy_size = new size;
-
+		{
+			copy_size = new_size;
+		}
 		memcpy(new_ptr, ptr, copy_size);
 		free(ptr);
 	}
