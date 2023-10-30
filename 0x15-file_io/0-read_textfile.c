3474f39ea4d0e29c083e5bfdbfa53d4dbb74cddb
goldenjeffempire <jeffemuodafe124@gmail.com>
2023-10-30 16:18:21 -0700

    Read textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+  * read_textfile - Read a text file and print it out
+  * @filename: The name of the file to read
+  * @letters: The number of letters to read and print
+  * Return: The actual number of letters and read printed,
+  *        or 0 on failure
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+		return (0);
+
+	int fd = open(filename, O_RDONLY);
+
+	if (fd == 1)
+		return (0);
+
+	char buffer[letters];
+	ssize_t bytes_read = read(fd, buffer, letters);
+
+	if (bytes_read == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
+	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	close(fd);
+
+	if (bytes_written == -1 || bytes_written != bytes_read)
+		return (0);
+
+	return (bytes_written);
+}
