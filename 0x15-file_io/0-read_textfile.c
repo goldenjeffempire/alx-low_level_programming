bbdd21b4ef32801c70172e8512ac6d3a8ae2b04f
goldenjeffempire <jeffemuodafe124@gmail.com>
2023-10-30 16:28:11 -0700

    Read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,36 @@
 #include "main.h"
 
 /**
-  * read_textfile - Read a text file and print it out
-  * @filename: The name of the file to read
-  * @letters: The number of letters to read and print
-  * Return: The actual number of letters and read printed,
-  *        or 0 on failure
-  */
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: The name of the file to read
+ * @letters: The number of letters to read and print
+ * Return: The actual number of leters read and printed,
+ *         Or, 0 on failure
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
+	ssize_t fo, fr, fw;
+	char *fbuf;
+
+	if (!filename)
 		return (0);
 
-	int fd = open(filename, O_RDONLY);
-
-	if (fd == 1)
+	fbuf = malloc(sizeof(char) * letters);
+	if (!fbuf)
 		return (0);
 
-	char buffer[letters];
-	ssize_t bytes_read = read(fd, buffer, letters);
+	fo = open(filename, O_RDONLY);
+	fr = read(fo, fbuf, letters);
+	fw = write(STDOUT_FILENO, fbuf, fr);
 
-	if (bytes_read == -1)
+	if (fo == -1 || fr == -1 || fw == -1 || fw != fr)
 	{
-		close(fd);
+		free(fbuf);
 		return (0);
 	}
 
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	free(fbuf);
+	close(fo);
 
-	close(fd);
-
-	if (bytes_written == -1 || bytes_written != bytes_read)
-		return (0);
-
-	return (bytes_written);
+	return (fw);
 }
