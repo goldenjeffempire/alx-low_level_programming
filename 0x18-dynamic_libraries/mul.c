399bb73942690e73506d684bd5940fe453b2be8c
goldenjeffempire <jeffemuodafe124@gmail.com>
2023-12-12 00:14:46 -0800

    Operations

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mul - Multiply two integers
+ * @a: first int
+ * @b: second int
+ * Return: Multiply result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
